3612815b713e38093de8eda5ce6181ffc6771b39
RogsGetaruelas-UCM <rogsgee@gmail.com>
2025-09-16 09:05:45 +0800

    Add files via upload

---
+++ Chapter_1/FavoriteSong.java
@@ -0,0 +1,8 @@
+public class FavoriteSong {
+    public static void main(String[] args) {
+        System.out.println("Darling i don't wish you well");
+        System.out.println("When you ain't with me");
+        System.out.println("I want you crying,");
+        System.out.println("I know it's wrong, but i just want to be honest");
+    }
+}
